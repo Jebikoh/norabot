78327c1b91d832674e0b8b62c8874d9cbd188165
Aneesh Edara <aneeshedara@gmail.com>
2018-08-21 01:43:56 -0400

    Added LoL User Command ~ Basic Functionality

---
+++ commands/lol-championRotation.js
@@ -0,0 +1,45 @@
+process.env.LEAGUE_API_PLATFORM_ID = 'na1'
+
+const Discord = require('discord.js');
+
+const { riotapi } = require('../config.json');
+
+const LeagueJs = require('leaguejs');
+const LeagueJsInstance = new LeagueJs(riotapi);
+
+module.exports = {
+    name: 'lol-user',
+    description: 'Get basic profile information',
+    aliases: ['lolusr'],
+    usage: `[command username]`,
+    execute(message, args) {
+        let summonerIcon, summonerName, summonerLevel;
+
+        LeagueJsInstance.Summoner
+            .gettingByName(args[0])
+            .then(data => {
+                'use strict';
+                console.log(data);
+                summonerIcon = data.profileIconId;
+                summonerName = data.name;
+                summonerLevel = data.summonerLevel;
+
+                let summonerIconURL = "http://ddragon.leagueoflegends.com/cdn/8.16.1/img/profileicon/" + summonerIcon + ".png";
+                console.log(summonerIconURL);
+
+                const embed = new Discord.RichEmbed()
+                    .setTitle(`**` + summonerName + `**`)
+                    .setColor(0x00AE86)
+                    .setThumbnail(summonerIconURL)
+                    .setTimestamp()
+                    .addField(`Level:` , summonerLevel.toString());
+
+                message.channel.send({embed});
+            })
+            .catch(err => {
+                'use strict';
+                console.log(err);
+                message.channel.send('Sorry, something went wrong! Either you entered your summoner name wrong, or something else happened. If the issue persists, contact a dev!')
+            });
+    }
+};
