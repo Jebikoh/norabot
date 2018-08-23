482983c5ddb50ddb5131031ca85392b64747e82b
nitroignika <aneeshedara@gmail.com>
2018-08-23 11:45:06 -0400

    Added license message to each file

--- commands/lol/lol-summonerInfo.js
+++ commands/lol/lol-summonerInfo.js
@@ -1,3 +1,27 @@
+/**
+ * norabot: a multi-purpose Discord bot
+ *
+ * Copyright (C) 2018 by nitroignika
+ *
+ * This file is part of norabot.
+ *
+ * norabot application is free software: you can redistribute
+ * it and/or modify it under the terms of the GNU Affero General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * norabot application is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU A General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with norabot.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @license AGPL-3.0+ <http://spdx.org/licenses/AGPL-3.0+>
+ */
+
+
 process.env.LEAGUE_API_PLATFORM_ID = 'na1'
 
 const Discord = require('discord.js');
